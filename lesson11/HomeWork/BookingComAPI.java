000bdad320e8302250d9dcfd4dca157646f1c5fd
nikzim <nicksecond1995@gmail.com>
2019-02-23 00:56:52 +0100

    &&

--- lesson11/HomeWork/BookingComAPI.java
+++ lesson11/HomeWork/BookingComAPI.java
@@ -10,7 +10,27 @@ public class BookingComAPI implements API {
 
     @Override
     public Room[] findRooms(int price, int persons, String city, String hotel) {
-        return new Room[0];
+
+        int apartments = 0;
+
+        for ( Room room: rooms){
+            if (room.getPersons() == persons && room.getCityName() == city && room.getHotelName() == hotel){
+                if (room.getPrice() >= price - 100 && room.getPrice() <= price + 100)
+                    apartments++;
+            }
+        }
+        Room[] arrayFindRoom = new Room[apartments];
+
+        apartments = 0;
+
+        for (Room room : rooms){
+            if (room.getPersons() == persons && room.getCityName() == city && room.getHotelName() == hotel){
+                if (room.getPrice() >= price - 100 && room.getPrice() <= price + 100)
+                    arrayFindRoom[apartments] = room;
+                apartments++;
+            }
+        }
+        return arrayFindRoom;
     }
 
 
