501ac7ee819533a61a1a8108afb2607325b0ee6d
nikzim <nicksecond1995@gmail.com>
2019-04-23 20:43:06 +0200

    &&

--- lesson18/HomeWork/Solution.java
+++ lesson18/HomeWork/Solution.java
@@ -6,7 +6,10 @@ public class Solution {
     public static void main(String[] args) {
 
         String str = new String("MacBook Air 2015 and 2017 or Pro 2019");
+        String str1 = new String("MacBook Pro 18 or RazerLaptop 19 or Surface 17");
         System.out.println(Arrays.toString(findNumbers(str)));
+        System.out.println("-------------------------------");
+        System.out.println(Arrays.toString(findNumbersNotTryCatch(str1)));
     }
 
 
@@ -30,6 +33,30 @@ public class Solution {
                 System.out.println("not a number");
             }
 
+        }
+        return result;
+    }
+
+    public static int[] findNumbersNotTryCatch(String text) {
+        String[] words = text.split(" ");
+
+        int count = 0;
+        for (String w : words) {
+            if (checkNumber(w))
+                count++;
+        }
+
+        int[] result = new int[count];
+        int j = 0;
+
+        for (String w : words) {
+            if (checkNumber(w)) {
+                result[j] = Integer.parseInt(w);
+                j++;
+            }
+            else {
+                System.out.println("not a number");
+            }
         }
         return result;
     }
