69f859eb6ebe0086e65af16d89c5ec17cee320c2
nikzim <nicksecond1995@gmail.com>
2019-05-02 02:25:02 +0200

    &&

---
+++ lesson13/debug/startDebug.java
@@ -0,0 +1,15 @@
+package lesson13.debug;
+
+public class startDebug {
+    public static void main(String[] args) {
+
+        Car car = new Car();
+        car.setName("Toyota");
+
+        car.setName("BMW");
+
+        car.setName(null);
+
+
+    }
+}
